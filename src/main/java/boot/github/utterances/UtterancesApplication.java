0637e0dd9b3efb0b3a3416424e45d8e82c8a5f08
KIM HONGIL <clroyan01@gmail.com>
2020-01-28 23:36:21 +0900

    Create utterances comments

---
+++ src/main/java/boot/github/utterances/UtterancesApplication.java
@@ -0,0 +1,13 @@
+package boot.github.utterances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtterancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtterancesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/github/utterances/UtterancesApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.github.utterances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtterancesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
